01914956b97eda318fc4f4bdd5f4a29b8f61f379
Ashot <macashot199514@gmail.com>
2019-03-08 22:11:06 +0400

    first commit

---
+++ todoApp/Globals.swift
@@ -0,0 +1,9 @@
+//
+//  Globals.swift
+//  todoApp
+//
+//  Created by Ashot on 3/6/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import Foundation
---
+++ todoApp/todoItems.swift
@@ -0,0 +1,9 @@
+//
+//  todoItems.swift
+//  todoApp
+//
+//  Created by Ashot on 3/6/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import Foundation
